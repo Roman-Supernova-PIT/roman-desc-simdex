c56fc6b2083659c419d1c939be95cc06357d6630
Rob Knop (Nersc) <raknop@lbl.gov>
2024-08-29 07:46:52 -0700

    production server

--- src/server.py
+++ src/server.py
@@ -62,8 +62,7 @@ class BaseView(flask.views.View):
             for arg in argstr.split("/"):
                 match = self.kwvalre.search( arg )
                 if match is None:
-                    app.logger.error( f"error parsing url argument {arg}, must be key=value" )
-                    return f'error parsing url argument {arg}, must be key=value', 500
+                    raise KeywordParseException( f"error parsing url argument {arg}, must be key=value" )
                 kw = match.group('k').strip()
                 val = match.group('v').strip()
                 parsedval = None
@@ -106,8 +105,7 @@ class BaseView(flask.views.View):
                         parsedval = val
 
                 if parsedval is None:
-                    app.logger.error( f"error parsing value \"{val}\"; this should never happen!" )
-                    return f"error parsing \"{val}\"; this should never happen!", 500
+                    raise KeywordParseException( f"error parsing value \"{val}\"; this should never happen!" )
 
                 app.logger.debug( f"keyword {kw} parsed to {parsedval} (type {type(parsedval)})" )
                 
@@ -115,102 +113,135 @@ class BaseView(flask.views.View):
                 
         if flask.request.is_json:
             kwargs.update( flask.request.json )
+
         return kwargs
 
 
-    def parse_kws_to_sql( self, data, fields=None, imagesearch=False, transientsearch=False ):
-        if fields is None:
-            if imagesearch == transientsearch:
-                raise ValueError( "Must either pass fields or set exactly one of (imagesearch,transientsearch)" )
-            if imagesearch:
-                fields = { 'pointing': { 'nums': { 'num', 'pointing_ra', 'pointing_dec', 'exptime', 'mjd', 'pa' },
-                                         'text': { 'filter' },
-                                         'abbrev': 'p',
-                                         'map': { 'pointing_ra': 'ra', 'pointing_dec': 'dec' },
-                                        },
-                           'sca': { 'nums': { 'scanum', 'ra', 'dec',
-                                              'ra_00', 'dec_00', 'ra_01', 'dec_01',
-                                              'ra_10', 'dec_10', 'ra_11', 'dec_11',
-                                              'minra', 'maxra', 'mindec', 'maxdec' },
-                                    'text': {},
-                                    'map': {},
-                                    'abbrev': 's'
-                                   }
-                          }
-            elif transientsearch:
-                fields = { 'transient': { 'nums': { 'id', 'healpix', 'ra', 'dec', 'host_id', 'gentype',
-                                                    'start_mjd', 'end_mjd', 'z_cmb', 'mw_ebv',
-                                                    'av', 'rv', 'v_pec', 'host_ra', 'host_dec',
-                                                    'host_mag_g', 'host_mag_i', 'host_mag_f',
-                                                    'host_sn_sep', 'peak_mjd',
-                                                    'peak_mag_g', 'peak_mag_i', 'peak_mag_f',
-                                                    'lens_dmu' },
-                                          'text': { 'model_name' },
-                                          'map': {},
-                                          'abbrev': 't'
-                                         }
-                          }
+    def parse_kws_to_sql( self, argstr, fieldspec=None, imagesearch=False, transientsearch=False, allfields=None ):
+        data = self.argstr_to_args( argstr )
+        if not isinstance( data, dict ):
+            app.logger.error( f"parse_kws_to_sql: data isn't a dict!  This shouldn't happen" )
+            raise Exception( f"parse_kws_to_sql: data isn't a dict!  This shouldn't happen" )
 
-        andtxt = ''
-        q = ''
-        subdict = {}
-        containing = False
-        ra = None
-        dec = None
-        
-        for kw, val in data.items():
-            # Special case: containing for an image search
-            if kw == 'containing':
-                app.logger.debug( f"Gonna check if {val} is a tuple or list of 2 ints/floats" )
-                if ( ( not ( isinstance(val, tuple) or isinstance(val, list) ) ) or ( len(val) != 2 )
-                     or ( not ( isinstance(val[0], float) or isinstance(val[0], int) ) )
-                     or ( not ( isinstance(val[1], float) or isinstance(val[1], int) ) )
-                    ):
-                    app.logger.error( f"Invalid containing: {val} (type {type(val)}, types {[type(i) for i in val]})" )
-                    raise KeywordParseException( f"containing must be a tuple or list with two decimal degree values" )
-                q += ( f' {andtxt} ( mindec<=%(dec)s AND maxdec>=%(dec)s '
-                       f'            AND '
-                       f'            ( maxra>minra AND minra<=%(ra)s AND maxra>=%(ra)s ) '
-                       f'            OR '
-                       f'            ( maxra<minra AND ( %(ra)<=maxra OR %(ra)>=minra ) ) ) ' )
+        if ( allfields is not None ) and ( 'fields' in data ):
+            if not set( data['fields'] ).issubset( set( allfields ) ):
+                diff = set( data['fields' ] ) - set( allfields )
+                app.logger.error( f"{self.__class__.__name__}: passed invalid fields {diff}" )
+                raise Exception( f"Invalid return fields: {diff}" )
+            fields = ",".join( data[ 'fields' ] )
+            del data['fields']
+        else:
+            fields = "*"
+            
+        try:
+            if fieldspec is None:
+                if bool(imagesearch) == bool(transientsearch):
+                    raise ValueError( "Must either pass fieldspec, "
+                                      "or set exactly one of (imagesearch,transientsearch)" )
+                if imagesearch:
+                    fieldspec = { 'pointing': { 'nums': { 'num', 'pointing_ra', 'pointing_dec',
+                                                          'exptime', 'mjd', 'pa' },
+                                                'text': { 'filter' },
+                                                'abbrev': 'p',
+                                                'map': { 'pointing_ra': 'ra', 'pointing_dec': 'dec' },
+                                               },
+                                  'sca': { 'nums': { 'scanum', 'ra', 'dec',
+                                                     'ra_00', 'dec_00', 'ra_01', 'dec_01',
+                                                     'ra_10', 'dec_10', 'ra_11', 'dec_11',
+                                                     'minra', 'maxra', 'mindec', 'maxdec' },
+                                           'text': {},
+                                           'map': {},
+                                           'abbrev': 's'
+                                          }
+                                 }
+                elif transientsearch:
+                    fieldspec = { 'transient': { 'nums': { 'id', 'healpix', 'ra', 'dec', 'host_id', 'gentype',
+                                                           'start_mjd', 'end_mjd', 'z_cmb', 'mw_ebv',
+                                                           'av', 'rv', 'v_pec', 'host_ra', 'host_dec',
+                                                           'host_mag_g', 'host_mag_i', 'host_mag_f',
+                                                           'host_sn_sep', 'peak_mjd',
+                                                           'peak_mag_g', 'peak_mag_i', 'peak_mag_f',
+                                                           'lens_dmu' },
+                                                 'text': { 'model_name' },
+                                                 'map': {},
+                                                 'abbrev': 't'
+                                                }
+                                 }
 
-                ra = val[0]
-                dec = val[1]
-                subdict['ra'] = ra
-                subdict['dec'] = dec
-                containing = True
-                andtxt = 'AND'
-                continue
+            andtxt = ''
+            q = ''
+            subdict = {}
+            containing = False
+            ra = None
+            dec = None
 
-            minmax = None
-            field = None
-            match = self.minmaxre.search( kw )
-            if match is not None:
-                minmax = match.group(2)
-                field = match.group(1)
-            else:
-                field = kw
+            for kw, val in data.items():
+                # Special case: containing for an image search
+                if kw == 'containing':
+                    app.logger.debug( f"Gonna check if {val} is a tuple or list of 2 ints/floats" )
+                    if ( ( not ( isinstance(val, tuple) or isinstance(val, list) ) ) or ( len(val) != 2 )
+                         or ( not ( isinstance(val[0], float) or isinstance(val[0], int) ) )
+                         or ( not ( isinstance(val[1], float) or isinstance(val[1], int) ) )
+                        ):
+                        app.logger.error( f"Invalid containing: {val} (type {type(val)}, "
+                                          f"types {[type(i) for i in val]})" )
+                        raise KeywordParseException( f"containing must be a tuple or list "
+                                                     f"with two decimal degree values" )
+                    q += ( f' {andtxt} ( mindec<=%(dec)s AND maxdec>=%(dec)s '
+                           f'            AND '
+                           f'            ( maxra>minra AND minra<=%(ra)s AND maxra>=%(ra)s ) '
+                           f'            OR '
+                           f'            ( maxra<minra AND ( %(ra)s<=maxra OR %(ra)s>=minra ) ) ) ' )
 
-            foundfield = False
-            for tab, tabinfo in fields.items():
-                if ( field in tabinfo['nums'] ) or ( field in tabinfo['text'] ):
-                    foundfield = True
-                    dbfield = field if field not in tabinfo['map'] else tabinfo['map'][field]
-                    abbrev = tabinfo['abbrev']
-                    if ( field in tabinfo['text'] ) and ( minmax is not None ):
-                        raise KeywordParseExcepotion( f"_min and _max invalid with field {field}" )
-                    var = f"{abbrev}_{field}{'_min' if minmax=='min' else '_max' if minmax=='max' else ''}"
-                    q += f' {andtxt} {abbrev}.{dbfield}'
-                    q += ">=" if minmax == "min" else "<=" if minmax == "max" else "="
-                    q += f"%({var})s "
-                    subdict[ var ] = val
+                    ra = val[0]
+                    dec = val[1]
+                    subdict['ra'] = ra
+                    subdict['dec'] = dec
+                    containing = True
                     andtxt = 'AND'
-                    break
+                    continue
 
-            if not foundfield:
-                raise KeywordParseException( f"Unknown search field {field}" )
+                minmax = None
+                field = None
+                match = self.minmaxre.search( kw )
+                if match is not None:
+                    minmax = match.group(2)
+                    field = match.group(1)
+                else:
+                    field = kw
 
-        return q, subdict, containing, ra, dec
+                foundfield = False
+                for tab, tabinfo in fieldspec.items():
+                    if ( field in tabinfo['nums'] ) or ( field in tabinfo['text'] ):
+                        foundfield = True
+                        dbfield = field if field not in tabinfo['map'] else tabinfo['map'][field]
+                        abbrev = tabinfo['abbrev']
+                        if ( field in tabinfo['text'] ) and ( minmax is not None ):
+                            raise KeywordParseExcepotion( f"_min and _max invalid with field {field}" )
+                        var = f"{abbrev}_{field}{'_min' if minmax=='min' else '_max' if minmax=='max' else ''}"
+                        q += f' {andtxt} {abbrev}.{dbfield}'
+                        q += ">=" if minmax == "min" else "<=" if minmax == "max" else "="
+                        q += f"%({var})s "
+                        subdict[ var ] = val
+                        andtxt = 'AND'
+                        break
+
+                if not foundfield:
+                    raise KeywordParseException( f"Unknown search field {field}" )
+
+            return q, subdict, fields, containing, ra, dec
+
+        except KeywordParseException as ex:
+            msg = "Failed to parse "
+            if argstr is not None:
+                msg += f"argument string \"{argstr}\" "
+            if ( argstr is not None ) and ( len(flask.request.data) > 0 ):
+                msg += "and "
+            if len(flask.request.data) > 0:
+                msg += f"POST data \"{str(flask.request.data)}\" "
+            msg += f": {str(ex)}"
+            app.logger.error( msg )
+            return msg, 500
             
     
 # ======================================================================
@@ -221,18 +252,18 @@ class MainPage(BaseView):
 
 # ======================================================================
 
-class FindImages(BaseView):
+class FindRomanImages(BaseView):
     def do_the_things( self, argstr=None ):
-        data = self.argstr_to_args( argstr )
-        if not isinstance( data, dict ):
-            app.logger.error( "FindImages: data isn't a dict!  This shouldn't happen" )
-            return f"FindImages: data isn't a dict!  This shouldn't happen", 500
+        allfields = [ 'pointingnum', 'borera', 'boredec', 'filter', 'exptime', 'mjd', 'pa',
+                      'scanum', 'ra', 'dec', 'ra_00', 'dec_00', 'ra_01', 'dec_01',
+                      'ra_10', 'dec_10', 'ra_11', 'dec_11', ]
 
-        try:
-            wheretxt, subdict, containing, ra, dec = self.parse_kws_to_sql( data, imagesearch=True )
-        except KeywordParseException as ex:
-            return f"Failed to parse {argstr}: {str(ex)}", 500
+        ( wheretxt, subdict, fields,
+          containing, ra, dec ) = self.parse_kws_to_sql( argstr, imagesearch=True, allfields=allfields )
 
+        if re.search( wheretxt, "^/s*$" ):
+            return "findimages failed: must include some search criteria", 500
+        
         q = ( "SELECT p.num AS pointingnum,p.ra AS borera,p.dec AS boredec,p.filter,p.exptime,p.mjd,p.pa,"
               "  s.scanum,s.ra,s.dec,s.ra_00,s.dec_00,s.ra_01,s.dec_01,s.ra_10,s.dec_10,s.ra_11,s.dec_11" )
         if containing:
@@ -249,9 +280,9 @@ class FindImages(BaseView):
             cursor.execute( q, subdict )
 
             if containing:
-                q = ( "SELECT * FROM temp_find_images WHERE "
-                      "q3c_poly_query(%(ra)s, %(dec)s, "
-                      "ARRAY[ra_00,dec_00, ra_01,dec_01, ra_11,dec_11, ra_10,dec_10])" )
+                q = ( f"SELECT {fields} FROM temp_find_images WHERE "
+                      f"q3c_poly_query(%(ra)s, %(dec)s, "
+                      f"ARRAY[ra_00,dec_00, ra_01,dec_01, ra_11,dec_11, ra_10,dec_10])" )
                 subdict = { 'ra': ra, 'dec': dec }
                 app.logger.debug( f"Sending query: {cursor.mogrify(q,subdict)}" )
                 cursor.execute( q, subdict )
@@ -268,29 +299,15 @@ class FindImages(BaseView):
 
 class FindTransients(BaseView):
     def do_the_things( self, argstr=None ):
-        data = self.argstr_to_args( argstr )
-        if not isinstance( data, dict ):
-            app.logger.error( "FindTransients: data isn't a dict!  This shouldn't happen" )
-            return f"FindTransients: data isn't a dict!  This shouldn't happen", 500
-
-        
         allfields = [ 'id', 'healpix', 'ra', 'dec', 'host_id', 'gentype', 'model_name',
                       'start_mjd', 'end_mjd', 'z_cmb', 'mw_ebv', 'mw_extinction_applied',
                       'av', 'rv', 'v_pec', 'host_ra', 'host_dec',
                       'host_mag_g', 'host_mag_i', 'host_mag_f', 'host_sn_sep',
                       'peak_mjd', 'peak_mag_g', 'peak_mag_i', 'peak_mag_f',
                       'lens_dmu', 'lens_dmu_applied', 'model_params' ]
-        if 'fields' in data:
-            if not set( data['fields'] ).issubset( set( allfields ) ):
-                diff = set( data['fields' ] ) - set( allfields )
-                app.logger.error( "FindTransients: passed invalid fields {diff}" )
-                return f"Invalid transient return fields: {diff}", 500
-            fields = ",".join( data[ 'fields' ] )
-            del data['fields']
-        else:
-            fields = "*"
 
-        wheretxt, subdict, _, _, _ = self.parse_kws_to_sql( data, transientsearch=True )
+        wheretxt, subdict, fields, _, _, _ = self.parse_kws_to_sql( argstr, transientsearch=True, allfields=allfields )
+
         q = f"SELECT {fields} FROM transient t WHERE {wheretxt}"
 
         with DB() as con:
@@ -317,8 +334,8 @@ app.add_url_rule( "/",
                   strict_slashes=False )
 
 rules = {
-    "/findimages": FindImages,
-    "/findimages/<path:argstr>": FindImages,
+    "/findromanimages": FindRomanImages,
+    "/findromanimages/<path:argstr>": FindRomanImages,
     "/findtransients": FindTransients,
     "/findtransients/<path:argstr>": FindTransients,
 }
